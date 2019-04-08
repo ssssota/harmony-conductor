30e2a2bff24e2f8a5725f4e4daf605d70242756d
ssssota <tommysouta@gmail.com>
2019-04-08 12:39:47 +0900

    add ServiceWorker

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 const hc = new HarmonyConductor()
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').then(() => {
+      console.log('ServiceWorker registration successful.')
+    }, err => {
+      console.log('ServiceWorker registration failed: ', err)
+    })
+  })
+}
